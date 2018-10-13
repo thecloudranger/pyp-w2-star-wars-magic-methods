a582acb29d7c5546cca6f80e50e4d7af2c178d1a
Mohammed Fazalullah <mfazal.q@gmail.com>
2018-10-13 10:51:43 +0800

    First Assignment submission

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,3 +1,13 @@
 class People(object):
+    light_side = True
+    
     def __init__(self, name, dark_side=False):
-        pass
+        self.name = name
+        self.dark_side = dark_side
+        self.light_side = not dark_side
+    
+    def __str__(self):
+        return self.name
+
+    def __call__(self):
+        return "Help me "+self.name+", you're my only hope."
